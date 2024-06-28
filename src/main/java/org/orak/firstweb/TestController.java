c495c260846bde4713e0caf650a5c5838da2eb61
Nurullah Orak <orak6884@gmail.com>
2024-06-28 13:53:38 +0300

    second web project

--- src/main/java/org/orak/firstweb/TestController.java
+++ src/main/java/org/orak/firstweb/TestController.java
@@ -1,6 +1,7 @@
 package org.orak.firstweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class TestController {
     @GetMapping("/hello")
-    public String hello(){
-        return "Nurullah Orak";
+    public User hello(){
+        User user =new User();
+        user.userName="nurullah.orak";
+        user.fullName="Nurullah Orak";
+        user.userId=1;
+        user.weight=75;
+        return user;
+    }
+
+    @GetMapping("/hello2/{userName}")
+    public User hello2(@PathVariable(name="userName")String userName){
+        User user=new User();
+        user.userName=userName;
+        user.fullName="Nurullah Orak";
+        user.userId=1;
+        user.weight=75;
+        return user;
     }
 }
---
+++ src/main/java/org/orak/firstweb/User.java
@@ -0,0 +1,8 @@
+package org.orak.firstweb;
+
+public class User {
+    public String userName;
+    public  String fullName;
+    public int userId;
+    public  int weight;
+}
